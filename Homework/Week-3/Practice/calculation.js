bee5984f8ee90bb56bff7c9a56011e9b6abe1315
annamani <annamanik.vit@gmail.com>
2025-03-19 11:30:02 +0100

    add

---
+++ Homework/Week-3/Practice/calculation.js
@@ -0,0 +1,10 @@
+// function calculateDogAge(age)
+// {
+//     let humanYears=age*7;
+//     //console.log("Dog's age of "+age+" is "+humanYears+ " in human years");
+//     alert("Your doggo is "+humanYears+" years old");
+//     return humanYears;
+
+// }
+// let dogsAge=calculateDogAge(2);
+// console.log("Dogs Age: "+dogsAge);

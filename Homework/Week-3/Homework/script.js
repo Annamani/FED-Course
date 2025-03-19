cd605e207c76dc8a10dcf44a82ac51afdfa75b4c
annamani <annamanik.vit@gmail.com>
2025-03-19 11:24:27 +0100

    final commit

---
+++ Homework/Week-3/Homework/script.js
@@ -0,0 +1,12 @@
+function foodOrder(restaurantName,foodName, totalPrice)
+{
+    // console.log("List of Ordered items: ");
+    // console.log("You order food from "+restaurantName);
+    // console.log("Your food from "+foodName);
+    // console.log("Your total price for the food  "+totalPrice);
+    // console.log("\n");
+   console.log("You are ordering an amount of "+totalPrice + "DKK "+foodName+" from " +restaurantName);
+}
+foodOrder("indianSpice","ButterChicken",180);
+foodOrder("Dominos","Spicy Chicken Pizza",100);
+foodOrder("KFC","Ultimate Savings Bucket",170);

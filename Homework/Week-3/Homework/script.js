36594305ea17c3113485e26b7dbe69ce1e764aa7
annamani <annamanik.vit@gmail.com>
2025-03-20 13:57:16 +0100

    final

--- Homework/Week-3/Homework/script.js
+++ Homework/Week-3/Homework/script.js
@@ -8,6 +8,7 @@ function foodOrder(restaurantName,foodName, totalPrice)
    console.log("You are ordering an amount of "+totalPrice + "DKK "+foodName+" from " +restaurantName);
 }
 
+
 //Function calling
 foodOrder("indianSpice","ButterChicken",180);
 foodOrder("Dominos","Spicy Chicken Pizza",100);

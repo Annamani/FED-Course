c096f25a5e02c3ee43a8f504bbefc1f3d9e615c9
annamani <annamanik.vit@gmail.com>
2025-03-25 12:59:45 +0100

    final changes

--- Homework/Week-3/Homework/script.js
+++ Homework/Week-3/Homework/script.js
@@ -1,10 +1,10 @@
 function foodOrder(restaurantName,foodName, totalPrice)
 {
-    // console.log("List of Ordered items: ");
-    // console.log("You order food from "+restaurantName);
-    // console.log("Your food from "+foodName);
-    // console.log("Your total price for the food  "+totalPrice);
-    // console.log("\n");
+    /*console.log("List of Ordered items: ");
+    console.log("You order food from "+restaurantName);
+    console.log("Your food from "+foodName);
+    console.log("Your total price for the food  "+totalPrice);
+    console.log("\n");*/
     console.log(`You are ordering an amount of  ${totalPrice} DKK  ${foodName} from ${restaurantName}`);
    //console.log("You are ordering an amount of "+totalPrice + "DKK "+foodName+" from " +restaurantName);
 }

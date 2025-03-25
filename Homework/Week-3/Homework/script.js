bf06934e73a771d2cae713aaffa8eb1471e04b4b
annamani <annamanik.vit@gmail.com>
2025-03-25 12:52:29 +0100

    final

--- Homework/Week-3/Homework/script.js
+++ Homework/Week-3/Homework/script.js
@@ -5,7 +5,8 @@ function foodOrder(restaurantName,foodName, totalPrice)
     // console.log("Your food from "+foodName);
     // console.log("Your total price for the food  "+totalPrice);
     // console.log("\n");
-   console.log("You are ordering an amount of "+totalPrice + "DKK "+foodName+" from " +restaurantName);
+    console.log(`You are ordering an amount of  ${totalPrice} DKK  ${foodName} from ${restaurantName}`);
+   //console.log("You are ordering an amount of "+totalPrice + "DKK "+foodName+" from " +restaurantName);
 }
 
 

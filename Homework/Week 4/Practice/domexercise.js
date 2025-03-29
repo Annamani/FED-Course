549eaf5fcb6063c66b8cf2a31d624f7cb1acfd97
annamani <annamanik.vit@gmail.com>
2025-03-29 14:27:46 +0100

    add

--- Homework/Week 4/Practice/domexercise.js
+++ Homework/Week 4/Practice/domexercise.js
@@ -1,10 +1,13 @@
 const header=document.querySelector('h2');
 header.innerText=" Hello Annamani";
 
+
 const firstElement=document.getElementById('mydiv');
 setTimeout(()=>{
     firstElement.innerText="Hello REDI Class";
 }, 2000);
+console.log(firstElement.textContent)
+
 
 const secondElement=document.getElementsByClassName('myclass');
 secondElement.innerText="I Love Coding";
@@ -13,5 +16,10 @@ const thirdElement=document.getElementsByTagName('div')[2];
 thirdElement.innerText="Coding for life";
 
 const fourthElement=document.querySelectorAll('.list-item');
-fourthElement.length;
-// console.log(fourthElement.length);
+console.log(fourthElement.length);
+
+const resultElement=document.getElementById('count-list');
+resultElement.innerText=fourthElement.length;
+
+
+const header=document.getElementsByName

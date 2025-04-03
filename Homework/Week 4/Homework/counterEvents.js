924586e5ef339ef9578916ecd00b93678005f5a5
annamani <annamanik.vit@gmail.com>
2025-04-03 11:00:43 +0200

    modified files

---
+++ Homework/Week 4/Homework/counterEvents.js
@@ -0,0 +1,28 @@
+const displayValue = document.getElementById("counterDiv");
+console.log(displayValue); 
+let count=0;
+const incrementValue = document.getElementById("increment");
+incrementValue.addEventListener("click",()=>{
+    if (count<30){
+        count++;
+    }
+    else{
+        alert("Counter cannot go above 30.");
+    }
+    displayValue.innerHTML = count;
+    console.log(count); 
+})
+const decrementValue = document.getElementById("decrement");
+decrementValue.addEventListener("click",()=>{
+    if (count>0) {
+        count--;  
+    } else {
+        alert("Counter cannot go below zero.");
+    }
+    displayValue.innerHTML = count;
+    console.log(count); 
+})                                  
+const resetValue = document.getElementById("reset");
+resetValue.addEventListener("click",()=>{
+    displayValue.innerHTML = 0;
+})
--- Homework/Week 4/Homework/counterOperations.js
+++ Homework/Week 4/Homework/counterOperations.js
@@ -1,20 +1,48 @@
-const displayValue = document.getElementById("counterDiv");
+/*const displayValue = document.getElementById("counterDiv");
 console.log(displayValue); 
 let count=0;
 // function incrementCounter() {
-//     count++;  
-//     displayValue.innerHTML = count;
-//     console.log(count); 
+//      if (count<30){
+    //     count++;
+    // }
+    // else{
+    //     alert("Counter cannot go above 30.");
+    // }
+    // displayValue.innerHTML = count;
+    // console.log(count);
 // }
-let incrementCounter=()=> displayValue.innerHTML = ++count;
+let incrementCounter=()=>{
+    if (count<30){
+        count++;
+    }
+    else{
+        alert("Counter cannot go above 30.");
+    }
+    displayValue.innerHTML = count;
+    console.log(count);   
+} 
 // console.log(incrementCounter);
 
 // function decrementCounter() {
-//     count--;  
+//     if (count>0) {
+//         count--;  
+//     } else {
+//         alert("Counter cannot go below zero.");
+//     }
 //     displayValue.innerHTML = count;
 //     console.log(count); 
 // }
-let decrementCounter=()=> displayValue.innerHTML = --count;
+let decrementCounter=()=> {
+    if (count> 0) {
+        count--;
+    }
+    else {
+        alert("Counter cannot go below zero.");
+    }
+    displayValue.innerHTML = count;
+    console.log(count);
+}
+//
 // console.log(decrementCounter);
 // console.log(decrementCounter);
 // function resetCounter() {
@@ -22,3 +50,5 @@ let decrementCounter=()=> displayValue.innerHTML = --count;
 // }
 const resetCounter=()=>displayValue.innerHTML = 0;
 // console.log(resetCounter);
+
+*/

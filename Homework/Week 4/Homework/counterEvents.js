d82fd5701dc2f43ce392fff7aaa4a6140b11e584
Tim <13306362+timmarschall@users.noreply.github.com>
2025-04-05 09:45:24 +0200

    feat: remove unused files and format everything

--- Homework/Week 4/Homework/counterEvents.js
+++ Homework/Week 4/Homework/counterEvents.js
@@ -1,28 +1,27 @@
 const displayValue = document.getElementById("counterDiv");
-let count=0;
+let count = 0;
 const incrementValue = document.getElementById("increment");
-incrementValue.addEventListener("click",()=>{
-    if (count<30){
-        count++;
-    }
-    else{
-        alert("Counter cannot go above 30.");
-    }
-    displayValue.innerHTML = count;
-    console.log(count); 
-})
+incrementValue.addEventListener("click", () => {
+  if (count < 30) {
+    count++;
+  } else {
+    alert("Counter cannot go above 30.");
+  }
+  displayValue.innerHTML = count;
+  console.log(count);
+});
 const decrementValue = document.getElementById("decrement");
-decrementValue.addEventListener("click",()=>{
-    if (count>0) {
-        count--;  
-    } else {
-        alert("Counter cannot go below zero.");
-    }
-    displayValue.innerHTML = count;
-    console.log(count); 
-})                                  
+decrementValue.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+  } else {
+    alert("Counter cannot go below zero.");
+  }
+  displayValue.innerHTML = count;
+  console.log(count);
+});
 const resetValue = document.getElementById("reset");
-resetValue.addEventListener("click",()=>{
-    count=0;
-    displayValue.innerHTML = count;
-})
+resetValue.addEventListener("click", () => {
+  count = 0;
+  displayValue.innerHTML = count;
+});

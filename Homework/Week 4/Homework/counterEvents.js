bbffe1a6f02c450fa7c3897a5a50edc87668e3d1
Tim <13306362+timmarschall@users.noreply.github.com>
2025-04-05 12:42:49 +0200

    refactor: more consistent naming

--- Homework/Week 4/Homework/counterEvents.js
+++ Homework/Week 4/Homework/counterEvents.js
@@ -1,33 +1,33 @@
 // First let's assign the variables to the elements in the HTML
-const displayValue = document.getElementById("counterDiv");
-const incrementValue = document.getElementById("increment");
-const decrementValue = document.getElementById("decrement");
-const resetValue = document.getElementById("reset");
+const counterElement = document.getElementById("counter");
+const incrementElement = document.getElementById("increment");
+const decrementElement = document.getElementById("decrement");
+const resetElement = document.getElementById("reset");
 
 // Then any variables we need to keep track of
 let count = 0;
 
-incrementValue.addEventListener("click", () => {
+incrementElement.addEventListener("click", () => {
   if (count < 30) {
     count++;
   } else {
     alert("Counter cannot go above 30.");
   }
-  displayValue.innerHTML = count;
+  counterElement.innerHTML = count;
   console.log(count);
 });
 
-decrementValue.addEventListener("click", () => {
+decrementElement.addEventListener("click", () => {
   if (count > 0) {
     count--;
   } else {
     alert("Counter cannot go below zero.");
   }
-  displayValue.innerHTML = count;
+  counterElement.innerHTML = count;
   console.log(count);
 });
 
-resetValue.addEventListener("click", () => {
+resetElement.addEventListener("click", () => {
   count = 0;
-  displayValue.innerHTML = count;
+  counterElement.innerHTML = count;
 });

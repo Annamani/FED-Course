0f02027673f1d8abd51affeb4583629f58865daf
annamani <annamanik.vit@gmail.com>
2025-04-17 23:02:47 +0200

    final changes added

--- Homework/Week-7/Homework/data.js
+++ Homework/Week-7/Homework/data.js
@@ -28,21 +28,25 @@ const teachers = [
 
 // console.log(students);
 // console.log(teachers);
+//Create a button to shuffle students and teachers array
+const button = document.createElement("button");
+button.innerText = "Assign students to teachers";
+document.body.appendChild(button);
+button.addEventListener("click", () => {
+  //shuffle students array
+  const shuffledStudents = students.sort(() => Math.random() - 0.5);
+  console.log(shuffledStudents);
+  //shuffle teachers array
+  const shuffledTeachers = teachers.sort(() => Math.random() - 0.5);
+  console.log(shuffledTeachers);
 
-//shuffle students array
-const shuffledStudents = students.sort(() => Math.random() - 0.5);
-console.log(shuffledStudents);
-//shuffle teachers array
-const shuffledTeachers = teachers.sort(() => Math.random() - 0.5);
-console.log(shuffledTeachers);
-
-//pick first student, pick first teacher and assign them.
-//Randomly assign students to teachers
-//save the result in a array of objects [ {...}, {...}, {...} ]
-const assignedStudents = students.map((shuffledStudents,index) => {
-  return {
-    student: shuffledStudents,
-    teacher: shuffledTeachers[index%shuffledTeachers.length],
-  };
+  //pick first student, pick first teacher and assign them.
+  //save the result in a array of objects [ {...}, {...}, {...} ]
+  const assignedStudents = students.map((shuffledStudents, index) => {
+    return {
+      student: shuffledStudents,
+      teacher: shuffledTeachers[index % shuffledTeachers.length],
+    };
+  });
+  console.log(assignedStudents);
 });
-console.log(assignedStudents);
